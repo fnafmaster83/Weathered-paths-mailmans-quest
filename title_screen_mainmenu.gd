8ea342f2e0db7c6a5f2a607e2975cbc3c176d37a
fnafmaster83 <rivrya74@kingshigh.school.nz>
2025-06-26 12:32:43 +1200

    85% lets go

--- title_screen_mainmenu.gd
+++ title_screen_mainmenu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
-	
+
 
 
 
@@ -15,3 +15,14 @@ func _on_texture_button_2_pressed() -> void:
 
 func _on_texture_button_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://world_3.tscn")
+
+
+
+
+func _on_hidden_exit_pressed() -> void:
+	get_tree().quit()
+
+
+
+func _on_texture_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://secret_level_jellyfish_road.tscn")
---
+++ world_3.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://victory_screen.tscn")
